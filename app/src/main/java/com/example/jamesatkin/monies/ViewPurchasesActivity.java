23cb42e6bbfcd9c706f498bb28b48f115d82bfdf
James Atkin <James.Atkin@infracast.com>
2017-07-17 16:55:34 +0100

    Done some work towards adding a purchase

---
+++ app/src/main/java/com/example/jamesatkin/monies/ViewPurchasesActivity.java
@@ -0,0 +1,13 @@
+package com.example.jamesatkin.monies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewPurchasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_purchases);
+    }
+}
